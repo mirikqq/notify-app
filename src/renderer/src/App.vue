4019c01fab59ebe17a2b7c58d708a15316e18322
mirikking <mrmirik.81@gmail.com>
2023-07-03 12:18:02 +0300

    v1

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,25 +1,17 @@
 <template>
-  <UserView>
-  </UserView>
+  <UserView> </UserView>
 </template>
-
-<script>
-  export default {
-    
-  }
-</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600&display=swap');
 
-  :root {
-    background-color: #242424;;
-    font-family: 'Montserrat', sans-serif;
-  }
+:root {
+  background-color: #242424;
+  font-family: 'Montserrat', sans-serif;
+}
 
-  * {
-    margin: 0;
-    padding: 0;
-  }
-
+* {
+  margin: 0;
+  padding: 0;
+}
 </style>
